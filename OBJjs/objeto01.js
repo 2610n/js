a59d65fac6c2fe4b4ad2bbc9bb08a4a8f9599fa8
Felippe Moreno <lucasvander47@gmail.com>
2023-02-01 22:50:12 -0300

    obj

---
+++ OBJjs/objeto01.js
@@ -0,0 +1,12 @@
+
+
+const obj_produto={ //objeto coleção de variáveis chave valor
+    produto:"Arroz",
+    preco:3.00
+    
+}
+console.log(obj_produto.preco);//atraves do ponto eu consigo acessar o valor do meu atributo
+console.log(obj_produto.produto);//atraves do ponto eu consigo acessar o valor do meu atributo
+
+const cliente={}
+cliente.nome="Lucas"; //estou criando um atributo dinamicamente 
